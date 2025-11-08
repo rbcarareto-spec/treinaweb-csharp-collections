bce5ae9a925b7fc17c9d1a9a7893964f7b7b2561
rbcarareto-spec <rb.carareto@gmail.com>
2025-11-08 18:37:06 -0300

    salvar

--- AulaArrays.cs
+++ AulaArrays.cs
@@ -34,38 +34,38 @@ public class AulaArrays
         alunos[1] = "Cleyson";
         imprimir(alunosCopia);
 
-        //List<ProdutoEntity> list = new List<ProdutoEntity>()
-        //{
-        //    new ProdutoEntity { Id = 1, Nome = "Mouse", Preco = 3.00m },
-        //    new ProdutoEntity { Id = 2, Nome = "Teclado", Preco = 5.00m },
-        //    new ProdutoEntity { Id = 3, Nome = "Monitor", Preco = 100.00m }
+        List<ProdutoEntity> list = new List<ProdutoEntity>()
+        {
+            new ProdutoEntity { Id = 1, Nome = "Mouse", Preco = 3.00m },
+            new ProdutoEntity { Id = 2, Nome = "Teclado", Preco = 5.00m },
+            new ProdutoEntity { Id = 3, Nome = "Monitor", Preco = 100.00m }
 
-        //};
-
-
-
-        //ProdutoEntity[] produtos = list.ToArray();
-
-        //foreach (var p in produtos)
-        //{
-        //    Console.WriteLine($"{p.Id} - {p.Nome} - R${p.Preco}");
-        //}
+        };
 
 
 
+        ProdutoEntity[] produtos = list.ToArray();
 
-        //// ?? Convertendo para dicion�rio (chave = Id, valor = Produto)
-        //Dictionary<int, ProdutoEntity> produtosDict = list.ToDictionary(p => p.Id);
+        foreach (var p in produtos)
+        {
+            Console.WriteLine($"{p.Id} - {p.Nome} - R${p.Preco}");
+        }
 
-        //// ?? Acessando um item pelo ID (chave)
-        //var produto = produtosDict[2]; // acessa o produto com Id = 2
-        //Console.WriteLine($"Produto {produto.Id}: {produto.Nome} - R${produto.Preco}");
 
-        //// ?? Percorrendo o dicion�rio
-        //foreach (var kvp in produtosDict)
-        //{
-        //    Console.WriteLine($"Key: {kvp.Key}, Nome: {kvp.Value.Nome}, Pre�o: {kvp.Value.Preco}");
-        //}
+
+
+        // ?? Convertendo para dicion�rio (chave = Id, valor = Produto)
+        Dictionary<int, ProdutoEntity> produtosDict = list.ToDictionary(p => p.Id);
+
+        // ?? Acessando um item pelo ID (chave)
+        var produto = produtosDict[2]; // acessa o produto com Id = 2
+        Console.WriteLine($"Produto {produto.Id}: {produto.Nome} - R${produto.Preco}");
+
+        // ?? Percorrendo o dicion�rio
+        foreach (var kvp in produtosDict)
+        {
+            Console.WriteLine($"Key: {kvp.Key}, Nome: {kvp.Value.Nome}, Pre�o: {kvp.Value.Preco}");
+        }
 
     }
 
